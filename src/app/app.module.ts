e968188b0d71459fe00d9de0f53c918caca29101
Silkens <Johan.Silkens@ordina.be>
2018-03-06 13:43:25 +0100

    draft overview layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StationListComponent } from './station-list/station-list.component';
+import { StationItemComponent } from './station-item/station-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StationListComponent,
+    StationItemComponent
   ],
   imports: [
     BrowserModule
